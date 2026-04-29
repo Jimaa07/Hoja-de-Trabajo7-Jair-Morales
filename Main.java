b3e401a3406933f00324e2f2ce3da3af8a0c40ff
Jimaa07 <jimafotos247@gmail.com>
2026-04-28 19:20:57 -0600

    main

--- Main.java
+++ Main.java
@@ -1,5 +1,28 @@
+import java.io.*;
+
 public class Main {
+
     public static void main(String[] args) {
-        
+        BinarySearchTree<Association<String, String>> tree = new BinarySearchTree<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader("diccionario.txt"))) {
+            String line;
+
+            while ((line = br.readLine()) != null) {
+                line = line.replace("(", "").replace(")", "");
+                String[] parts = line.split(",");
+
+                String english = parts[0].trim().toLowerCase();
+                String spanish = parts[1].trim().toLowerCase();
+
+                tree.add(new Association<>(english, spanish));
+            }
+
+        } catch (IOException e) {
+            System.out.println("Error leyendo diccionario");
+        }
+
+        System.out.println("Diccionario ordenado:");
+        tree.inOrder();
     }
 }
